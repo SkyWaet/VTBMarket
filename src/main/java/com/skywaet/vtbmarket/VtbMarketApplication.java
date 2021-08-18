1befee61d438e31c7b7d27f16ef1f855f7db4f85
SkyWaet <gslepenkov@gmail.com>
2021-08-18 12:49:07 +0300

    initial commit

---
+++ src/main/java/com/skywaet/vtbmarket/VtbMarketApplication.java
@@ -0,0 +1,13 @@
+package com.skywaet.vtbmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtbMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VtbMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skywaet/vtbmarket/VtbMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skywaet.vtbmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtbMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
